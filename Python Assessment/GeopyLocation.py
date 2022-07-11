be63b380d63bb5c5fb5f403f267bc688f49b9d21
Lokesh Singh Rajwar <lokeshsingh@codez.in>
2022-07-12 02:17:33 +0530

    (11/07/22)

--- Python Assessment/GeopyLocation.py
+++ Python Assessment/GeopyLocation.py
@@ -3,9 +3,9 @@
 ##Below program illustrates how to calculate great-circle distance from latitude-longitude data.
 
 print("We can locate any place on earth from its geographic coordinates.")
-from geopy.distance import geodesic as dist
+from geopy.distance import great_circle as GC
    
-data = [(26.19339, 73.11566), (25.98418, 73.35779), (18.66547, 81.13568)]
+data = [(34.15191, 77.57611), (34.15124, 77.57662), (34.15057, 77.57714), (34.14958, 77.57800), (34.14859, 77.57886), (34.14729, 77.58000), (34.14599, 77.58114), (34.14405, 77.58283), (34.14211, 77.5845)]
 
 def total_distance(lst):
 
@@ -20,10 +20,10 @@ def total_distance(lst):
         coords_2 = lst[i+1]
         i += 1
         x -= 1
-        d += dist(coords_1,coords_2).km
+        d += GC(coords_1,coords_2).meters
     return d
 
-print(f"The Total distance would be: {total_distance(data)} Km")
+print(f"The Total distance would be: {total_distance(data)} Meters")
 
 ## We can locate any place on earth from its geographic coordinates.
 ## The Total distance would be: 1149.2745548322673 Km
