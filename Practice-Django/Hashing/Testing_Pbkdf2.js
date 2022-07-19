a4536fba31698d3ac9d031c83d31dac362f3552d
Lokesh Singh Rajwar <lokeshsingh@codez.in>
2022-07-19 11:59:48 +0530

    Django Hashing

---
+++ Practice-Django/Hashing/Testing_Pbkdf2.js
@@ -0,0 +1,15 @@
+const djangoHash = require('/home/lokeshsingh/Documents/Repos/Python-Django/Practice-Django/Hashing/Crypto_Pbkdef2.js');
+
+const password = 'p@ssw0rd';
+
+djangoHash.hash(password)
+  .then(hash => {
+    console.log('Hashed: ', hash);
+
+    djangoHash.verify(password, hash)
+      .then(result => {
+        console.log('Verify: ', result);
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
