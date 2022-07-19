7699b234dce1fd5ef8853e08de10cecbb38c8ec4
Lokesh Singh Rajwar <lokeshsingh@codez.in>
2022-07-19 12:08:12 +0530

    Requests Update

---
+++ Practice-Django/Hashing/js password encryption/check.js
@@ -0,0 +1,29 @@
+var pbkdf2 = require("pbkdf2");
+
+const password = "Skmd@9055";
+
+//const djangoPassword ="pbkdf2_sha256$320000$EkAhjshyeYYPvI3z9r12AB$KVvNP2lMpgx//MxJASzlwqJ5WrtdwnNxZaoYH3ybPMA=";
+
+const djangoPassword2 ="pbkdf2_sha256$320000$FbaZqRTtEUEVljgkwWDUPg==$up4V0DYda4tWqAd4WPLx8JY+77THQpJB1sFmJVtcOyQ="
+
+var validatePassword = function(key, string) {
+  var parts = string.split("$");
+  var iterations = parts[1];
+  var salt = parts[2];
+  const hashPassword = pbkdf2
+    .pbkdf2Sync(key, Buffer.from(salt), Number(iterations), 32, "sha256")
+    .toString("base64");
+
+  console.log(`parts == ${parts}`)
+  console.log(`iterations == ${iterations}`)
+  console.log(`salt == ${salt}`)
+  console.log(`hashPassword == ${hashPassword}`)
+
+  return hashPassword === parts[3];
+};
+
+console.log(validatePassword(password, djangoPassword2));
+
+//a5fcec9ebb0c158254463e
+//FbaZqRTtEUEVljgkwWDUPg==
+//SA7IXkI8kwwDucy09++8ilhD8pdb1MwvDuvGJ7wMq6Y=
