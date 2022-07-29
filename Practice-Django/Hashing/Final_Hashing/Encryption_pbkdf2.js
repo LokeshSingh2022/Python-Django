94eb131431629a00c52fec6b2f1468140db68575
Lokesh Singh Rajwar <lokeshsingh@codez.in>
2022-07-29 21:07:06 +0530

    Finally Updated

--- Practice-Django/Hashing/Final_Hashing/Encryption_pbkdf2.js
+++ Practice-Django/Hashing/Final_Hashing/Encryption_pbkdf2.js
@@ -1,27 +1,26 @@
 const crypto = require('crypto')
 
+    const pass = "12345_Hashing"
+    const keyLength = 32
+    const hashAlg = 'sha256'
+    const iterations =  320000
 
-module.exports = {
+    
 
-    keyLength: 32,
-    hashAlg: 'sha256',
-    iterations: 320000,
-
-
-    hashPassword(plainText) {
+    var hashPassword = function(plainText) {
         let salt = crypto.randomBytes(32).toString('base64')
         let pbkdf2 = crypto.pbkdf2Sync(
             plainText,
             salt,
-            this.iterations,
-            this.keyLength,
-            this.hashAlg
+            iterations,
+            keyLength,
+            hashAlg
         ).toString('base64')
         
-        return `${"pbkdf2_sha256"}$${this.iterations}$${salt}$${pbkdf2}`
-    },
+        return `${"pbkdf2_sha256"}$${iterations}$${salt}$${pbkdf2}`
+    }
    
-    verifyPassword(givenPassword, hashedPassword) {
+    var verifyPassword = function(givenPassword, hashedPassword) {
         let splits = hashedPassword.split('$')
         
         let hashAlg = 'sha256'
@@ -39,6 +38,8 @@ module.exports = {
         ).toString('base64')
 
         return testPbkdf2 === pbkdf2
-    },
+    }
 
-}
+console.log("Hashed Password:- " + hashPassword(pass));
+
+console.log("Result is:- " + verifyPassword(pass, hashPassword(pass)))
