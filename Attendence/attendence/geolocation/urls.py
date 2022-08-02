28dd67611f45969ee86ebacfecb27d48547f11cd
Lokesh Singh Rajwar <lokeshsingh@codez.in>
2022-08-02 20:07:07 +0530

    Geo Maps

--- Attendence/attendence/geolocation/urls.py
+++ Attendence/attendence/geolocation/urls.py
@@ -1,6 +1,6 @@
-# from django.urls import path
-# from .views import *
+from django.urls import path
+from .views import *
 
-# urlpatterns = [
-   
-# ]
+urlpatterns = [
+   path('', home, name='home')
+]
